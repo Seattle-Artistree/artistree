59a365492a0f300c141f3f8ab5dbc3debc0a36d2
Ana Ortega <anaa0hh@icloud.com>
2018-04-28 15:57:37 -0700

    pages working for single page app

--- public/scripts/controllers/routes.js
+++ public/scripts/controllers/routes.js
@@ -1,4 +1,5 @@
-
+'use strict';
+// var app = app || {};
 // all route changes should hide page containers
 page('/*', (ctx, next) => {
   $('.page').hide();
@@ -7,22 +8,8 @@ page('/*', (ctx, next) => {
 
 // page('/', () => console.log('hola'));
 
-page('/', app.bookView.initIndexPage);
+page('/', () => app.landingView.init());
+page('/home', () => app.homeView.init());
+page('/about', () => app.aboutView.init());
 
-page('/create', app.createBookView.initCreatePage);
-
-page('/:book_id', (ctx) => {
-
-  app.Book.fetchOne(ctx.params.book_id)
-    .then((singleBook) => {
-      app.singleBookView.initSinglePage(singleBook);
-    });
-});
-
-
-
-
-// page('*', app.bookView.initIndexPage);
-// page('/books/new', app.createPage.init);
-
-page.start();
+page();
