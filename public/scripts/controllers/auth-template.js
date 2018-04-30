55a11ff4b723a8bc8e0ffe456967b6d884acba5a
Haley Mendoza <mendozahaley@gmail.com>
2018-04-30 12:10:01 -0700

    removed flair.json. added necessary dependencies. grabbing data for top artists.

--- public/scripts/controllers/auth-template.js
+++ public/scripts/controllers/auth-template.js
@@ -1,31 +1,26 @@
-
-(function() {
+export var accessToken = '';
+export var allArtists = [];
+export default function () {
   /**
    * Obtains parameters from the hash of the URL
    * @return Object
    */
   function getHashParams() {
     var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-      q = window.location.hash.substring(1);
-    while ( e = r.exec(q)) {
+    var e, r = /([^&;=]+)=?([^&;]*)/g, q = window.location.hash.substring(1);
+    while (e = r.exec(q)) {
       hashParams[e[1]] = decodeURIComponent(e[2]);
     }
     return hashParams;
   }
-  var userProfileSource = document.getElementById('user-profile-template').innerHTML,
-    userProfileTemplate = Handlebars.compile(userProfileSource),
-    userProfilePlaceholder = document.getElementById('user-profile');
-  var oauthSource = document.getElementById('oauth-template').innerHTML,
-    oauthTemplate = Handlebars.compile(oauthSource),
-    oauthPlaceholder = document.getElementById('oauth');
+  var userProfileSource = document.getElementById('user-profile-template').innerHTML, userProfileTemplate = Handlebars.compile(userProfileSource), userProfilePlaceholder = document.getElementById('user-profile');
+  var oauthSource = document.getElementById('oauth-template').innerHTML, oauthTemplate = Handlebars.compile(oauthSource), oauthPlaceholder = document.getElementById('oauth');
   var params = getHashParams();
-  var access_token = params.access_token,
-    refresh_token = params.refresh_token,
-    error = params.error;
+  var access_token = params.access_token, refresh_token = params.refresh_token, error = params.error;
   if (error) {
     alert('There was an error during the authentication');
-  } else {
+  }
+  else {
     if (access_token) {
       // render oauth info
       oauthPlaceholder.innerHTML = oauthTemplate({
@@ -37,24 +32,27 @@
         headers: {
           'Authorization': 'Bearer ' + access_token
         },
-        success: function(response) {
+        success: function (response) {
+          accessToken = access_token;
+          allArtists.push(response);
           userProfilePlaceholder.innerHTML = userProfileTemplate(response);
           $('#login').hide();
           $('#loggedin').show();
         }
       });
-    } else {
+    }
+    else {
       // render initial screen
       $('#login').show();
       $('#loggedin').hide();
     }
-    document.getElementById('obtain-new-token').addEventListener('click', function() {
+    document.getElementById('obtain-new-token').addEventListener('click', function () {
       $.ajax({
         url: '/refresh_token',
         data: {
           'refresh_token': refresh_token
         }
-      }).done(function(data) {
+      }).done(function (data) {
         access_token = data.access_token;
         oauthPlaceholder.innerHTML = oauthTemplate({
           access_token: access_token,
@@ -63,4 +61,4 @@
       });
     }, false);
   }
-})();
+}
--- public/scripts/tree.js
+++ public/scripts/tree.js
@@ -1,4 +1,76 @@
-'use strict';
+//import getAuthInstance from './controllers/auth-template';
+var accessToken = '';
+var artists = [];
+
+(function() {
+  /**
+   * Obtains parameters from the hash of the URL
+   * @return Object
+   */
+  function getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g, q = window.location.hash.substring(1);
+    while (e = r.exec(q)) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+  var userProfileSource = document.getElementById('user-profile-template').innerHTML, userProfileTemplate = Handlebars.compile(userProfileSource), userProfilePlaceholder = document.getElementById('user-profile');
+  var oauthSource = document.getElementById('oauth-template').innerHTML, oauthTemplate = Handlebars.compile(oauthSource), oauthPlaceholder = document.getElementById('oauth');
+  var params = getHashParams();
+  var access_token = params.access_token, refresh_token = params.refresh_token, error = params.error;
+  if (error) {
+    alert('There was an error during the authentication');
+  }
+  else {
+    if (access_token) {
+      // render oauth info
+      oauthPlaceholder.innerHTML = oauthTemplate({
+        access_token: access_token,
+        refresh_token: refresh_token
+      });
+      $.ajax({
+        url: 'https://api.spotify.com/v1/artists/5BcAKTbp20cv7tC5VqPFoC',
+        headers: {
+          'Authorization': 'Bearer ' + access_token
+        },
+        success: function (response) {
+          accessToken = access_token;
+          console.log(response);
+          var id = response.id;
+          var name = response.name;
+          var image = response.images[0].url;
+          var topArtist = new Artist(id, name, image);
+          console.log('topArtist', topArtist);
+          artists.push(topArtist);
+          userProfilePlaceholder.innerHTML = userProfileTemplate(response);
+          $('#login').hide();
+          $('#loggedin').show();
+        }
+      });
+    }
+    else {
+      // render initial screen
+      $('#login').show();
+      $('#loggedin').hide();
+    }
+    document.getElementById('obtain-new-token').addEventListener('click', function () {
+      $.ajax({
+        url: '/refresh_token',
+        data: {
+          'refresh_token': refresh_token
+        }
+      }).done(function (data) {
+        access_token = data.access_token;
+        oauthPlaceholder.innerHTML = oauthTemplate({
+          access_token: access_token,
+          refresh_token: refresh_token
+        });
+      });
+    }, false);
+  }
+})();
+
 var margin = {top: 20, right: 120, bottom: 20, left: 120},
   width = 960 - margin.right - margin.left,
   height = 800 - margin.top - margin.bottom;
@@ -19,55 +91,39 @@ var svg = d3.select('#loggedin').append('svg')
   .append('g')
   .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-
-var allArtists = [
-  { id: '000001',
-    name: 'Jain',
-    image: 'https://i.scdn.co/image/6e4d8ba95cb31c0475179d55c2af4760136d304f',
-    children: [
-      {
-        id: '000002',
-        name: 'Lady Gaga',
-        image: 'https://i.scdn.co/image/5210a7fa24a58b3bc8109082fa7292afe437458f',
-        children: [
-          {
-            id: '3881838',
-            name: 'Gwen Stefani',
-            image: 'https://i.scdn.co/image/82a1aaedb700c8e13ae91e54c2c3329e1839c7ca',
-            children: []
-          }
-        ]
-      },
-      {
-        id: '000003',
-        name: 'Katy Perry',
-        image: 'https://i.scdn.co/image/fcdc433e8ccf8d46d58ac70db322feb9b3328731',
-        children: []
-      }
-    ]
-  }
-];
-
-// d3.json('flare.json', function(error, flare) {
-//   if (error) throw error;
-
-//   root = flare;
-//   root.x0 = height / 2;
-//   root.y0 = 0;
-
-//   function collapse(d) {
-//     if (d.children) {
-//       d._children = d.children;
-//       d._children.forEach(collapse);
-//       d.children = null;
-//     }
+// var allArtists = [
+//   { id: '000001',
+//     name: 'Jain',
+//     image: 'https://i.scdn.co/image/6e4d8ba95cb31c0475179d55c2af4760136d304f',
+//     children: [
+//       {
+//         id: '000002',
+//         name: 'Lady Gaga',
+//         image: 'https://i.scdn.co/image/5210a7fa24a58b3bc8109082fa7292afe437458f',
+//         children: [
+//           {
+//             id: '3881838',
+//             name: 'Gwen Stefani',
+//             image: 'https://i.scdn.co/image/82a1aaedb700c8e13ae91e54c2c3329e1839c7ca',
+//             children: []
+//           }
+//         ]
+//       },
+//       {
+//         id: '000003',
+//         name: 'Katy Perry',
+//         image: 'https://i.scdn.co/image/fcdc433e8ccf8d46d58ac70db322feb9b3328731',
+//         children: []
+//       }
+//     ]
 //   }
+// ];
 
-//   root.children.forEach(collapse);
-//   update(root);
-// });
-
-root = allArtists[0];
+console.log("artists", artists);
+top = artists[0];
+root = top;
+console.log('root', root);
+console.log('root-id', root.id);
 root.x0 = height / 2;
 root.y0 = 0;
 
@@ -79,7 +135,9 @@ function collapse(d) {
   }
 }
 
-root.children.forEach(collapse);
+if (root.children) {
+  root.children.forEach(collapse);
+}
 update(root);
 
 d3.select(self.frameElement).style('height', '800px');
@@ -97,29 +155,20 @@ function update(source) {
   var node = svg.selectAll('g.node')
     .data(nodes, function(d) { return d.id || (d.id = ++i); });
 
+  //get related artists
+  console.log('node', d3.select(node).datum());
+  console.log('source-id', node.id);
+  getRelatedArtists(node.id);
+
   // Enter any new nodes at the parent's previous position.
   var nodeEnter = node.enter().append('g')
     .attr('class', 'node')
     .attr('transform', function(d) { return 'translate(' + source.x0 + ',' + source.y0 + ')'; })
     .on('click', click);
 
-  // nodeEnter.append('circle')
-  //   .attr('r', 1e-6)
-  //   .style('fill', function(d) { return d._children ? 'lightsteelblue' : '#fff'; });
-
-  node.append('circle')
+  nodeEnter.append('circle')
     .attr('r', 1e-6)
-    .style('fill', 'transparent') // this code works OK
-    .style('stroke', 'black') // displays small black dot
-    .style('stroke-width', 0.25)
-    .on('mouseover', function(){ // when I use .style("fill", "red") here, it works
-      d3.select(this)
-        .style('fill', function(d) { return d.image; });
-    })
-    .on('mouseout', function(){
-      d3.select(this)
-        .style('fill', 'transparent');
-    });
+    .style('filter', function(d) { return d.image; });
 
 
   nodeEnter.append('text')
@@ -143,7 +192,7 @@ function update(source) {
 
   nodeUpdate.select('circle')
     .attr('r', 50)
-    .style('fill', function(d) { return d._children ? 'lightsteelblue' : '#fff'; });
+    .style('filter', function(d) { return d.image; });
 
   nodeUpdate.select('text')
     .style('fill-opacity', 1);
@@ -193,6 +242,29 @@ function update(source) {
   });
 }
 
+function getRelatedArtists(artistId) {
+  console.log("artistId", artistId);
+  $.ajax({
+    url: 'https://api.spotify.com/v1/artists/' + artistId + '/related-artists',
+    headers: {
+      'Authorization': 'Bearer ' + accessToken
+    },
+    success: function (response) {
+      //accessToken = access_token;
+      console.log(response);
+      // var id = response.id;
+      // var name = response.name;
+      // var image = response.images[0].url;
+      // var topArist = new Artist(id, name, image);
+      // console.log('topArtist', topArist);
+      // allArtists.push(topArist);
+      // userProfilePlaceholder.innerHTML = userProfileTemplate(response);
+      $('#login').hide();
+      $('#loggedin').show();
+    }
+  });
+}
+
 // Toggle children on click.
 function click(d) {
   if (d.children) {
@@ -202,5 +274,15 @@ function click(d) {
     d.children = d._children;
     d._children = null;
   }
+  //console.log("d", d);
+  //getRelatedArtists(d.id);
   update(d);
 }
+
+function Artist(id, name, image) {
+  this.id = id;
+  this.name = name;
+  this.image = image;
+  this.children = [];
+}
+//console.log(api_response.items[0].images[0].url);
