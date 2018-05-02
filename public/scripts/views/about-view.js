5fbfbcdcea6c197462109ef46045c4daca107122
Nicholas Welch <nicholaswelch1989@gmail.com>
2018-05-01 21:52:22 -0700

    remove some form too much to do little time use as strech goal

--- public/scripts/views/about-view.js
+++ public/scripts/views/about-view.js
@@ -5,26 +5,7 @@ var app = app || {};
   const aboutView = {};
 
   aboutView.init = () => $('#about-view').show();
-
-  // //store feedback ??
-  // app.Feedback.prototype.init = () => {
-
   
-  //   $('#row').off().on('submit', 'form', (event) => {
-  //     event.preventDefault();
-  //     const selection = $('#radio-inline').val('');
-  //     const user_id = $('').val('');
-  //     const comment = $('#comments').val('');
-  //     const date = newDate $(.now());
-  //     app.Feedback.create({ selection }, { user_id }, { comment }, {date});
-
-  //     console.log('posted');
-  //   });
-
-  // };
-
-    const createFeedback = {};
-
   module.aboutView = aboutView;
 
 })(app);
