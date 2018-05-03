f6419ff5bfb79b441e808e1e383202cb3d030e82
Nicholas Welch <nicholaswelch1989@gmail.com>
2018-05-02 19:04:11 -0700

    trying to get sql to work

--- server.js
+++ server.js
@@ -174,15 +174,47 @@ app.get('/refresh_token', function(req, res) {
 
 app.get('/test', (req, res) => res.send('It\'s working!'));
 
-// post to the database
-// app.post('/feedback', (request, response) => {
-//   client.query(
-//     'INSERT INTO feedback(feedback, "feedback") VALUES($1, $2) ON CONFLICT DO NOTHING',
-//     [request.body.feedback, request.body.feedback],
-//     function (err) {
-//       if (err) console.error(err)
-//     }
-//   )
+//Database About
+
+
+app.post('/index', (request, response) => {
+  client.query(
+    'INSERT INTO feedback(feedback, "username") VALUES($1, $2) ON CONFLICT SET TO NULL,
+    [request.body.feedback, request.body.username],
+    function (err) {
+      if (err) console.error(err)
+      queryTwo()
+    }
+  )
+
+  function queryTwo() {
+    client.query(
+      `SELECT user_id FROM feedback WHERE username=$1`,
+      [request.body.feedback],
+      function (err, result) {
+        if (err) console.error(err)
+        queryThree(result.rows[0].user_id)
+      }
+    )
+  }
+
+  function queryThree(user_id) {
+    client.query(
+      `INSERT INTO
+      feedback(user_id, comment, "publishedOn")
+      VALUES ($1, $2, $3);`,
+      [
+        user_id,
+        request.body.comment,
+        request.body.publishedOn,
+      ],
+      function (err) {
+        if (err) console.error(err);
+        response.send('insert complete');
+      }
+    );
+  }
+});
 
 // let url = 'https://api.spotify.com/v1/me/top/artists/';
 
